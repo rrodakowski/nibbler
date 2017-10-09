6a03eeb91dff8e0b874f4ca7308b763476c3cf1a
Randall Rodakowski <randall.rodakowski@thomsonreuters.com>
2017-10-09 15:56:35 -0400

    oops...screwed up sub dir name

--- nibbler/nibbler.py
+++ nibbler/nibbler.py
@@ -388,7 +388,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='nibbler', description='A simple RSS to email application.')
     parser.add_argument('to_email', metavar='to_email', help='To email address; youremail@example.com')
     parser.add_argument('from_email', metavar='from_email', help='From email address; nibble@example.com')
-    parser.add_argument('sub-dir', metavar='sub_dir', help='path to subscriptions.xml file')
+    parser.add_argument('sub_dir', metavar='sub_dir', help='path to subscriptions.xml file')
     parser.add_argument('-l', '--log-dir', metavar='log_dir', help='path to log dir')
     parser.add_argument('-d', '--db-dir', metavar='db_dir', help='path to sqlite db dir')
     parser.add_argument('-e', '--email-dir', metavar='email_dir', help='path to directory where email file is output before sending')
